8ec9ca79dfdfef5791797f968d8d78cc0ed60936
Tom Evans <tevans@odin>
2014-09-26 08:20:39 +0100

    Syncing minor changes to wfc3_routines.py

--- spectroscopy/wfc3_routines.py
+++ spectroscopy/wfc3_routines.py
@@ -11,18 +11,27 @@ import atpy
 def extract_spatscan_specra( images, ap_radius=60 ):
 
     # TODO - wrap this up as spectroscopy object somehow
-    ncross, ndisp, nimages = np.shape( images ) # maybe build this into object
-    spectra = np.zeros( [ nimages, ndisp ] )
-    cross_centers = np.zeros( nimages )
+    cross_axis = 0
+    disp_axis = 1
+    frame_axis = 2
+    z = np.shape( images ) # maybe build this into object
+    ncross = z[cross_axis]
+    ndisp = z[disp_axis]
+    nframes = z[frame_axis]
+    spectra = np.zeros( [ nframes, ndisp ] )
+    cross_centers = np.zeros( nframes )
     x = np.arange( ncross )
     
-    for i in range( nimages ):
+    print '\nExtracting spectra from 2D images:'
+    for i in range( nframes ):
+        print '... image {0} of {1}'.format( i+1, nframes )
+        image = images[:,:,i]
         # Extract the cross-dispersion profile, i.e. along
         # the axis of the spatial scan:
-        cross_profile = np.sum( images[i], axis=1 )
+        cross_profile = np.sum( image, axis=disp_axis )
         # Determine the center of the scan by taking the
         # point midway between the edges:
-        threshold = 0.2*np.median( spatial_profile )
+        threshold = 0.2*np.median( cross_profile )
         ixs = ( cross_profile>threshold )
         cross_centers[i] = np.mean( x[ixs] )
         # Determine the cross-dispersion coordinates between
@@ -35,13 +44,14 @@ def extract_spatscan_specra( images, ap_radius=60 ):
         xmin_full = int( np.ceil( xmin ) )
         xmax_full = int( np.floor( xmax ) )
         ixs_full = ( x>=xmin_full )*( x<=xmax_full )
-        spectra[i,:] = np.sum( images[ixs_full,:,i] )
+        spectra[i,:] = np.sum( image[ixs_full,:], axis=cross_axis )
         # Determine any rows that are partially contained
         # within the aperture at either end of the scan and
         # add their weighted contributions to the spectrum:
         xlow_partial = xmin_full - xmin
-        spectra[:,i] += images[xlow_partial,:,i]
+        spectra[i,:] += image[xlow_partial,:]
         xupp_partial = xmax - xmax_full
-        spectra[:,i] += images[xupp_partial,:,i]
+        spectra[i,:] += image[xupp_partial,:]
+    pdb.set_trace()
 
     return spectra
